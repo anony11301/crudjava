b0cb2166da59cd66a0734b759a6457b3a211762e
anony11301 <61984257+anony11301@users.noreply.github.com>
2023-07-18 08:50:51 +0700

    Add files via upload

---
+++ crud/src/koneksi/Koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author fredr
+ */
+public class Koneksi {
+    Connection koneksi;
+    
+    public static Connection Koneksi() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/crudjava", "root", "");
+            
+            return koneksi;
+        }catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            
+            return null;
+        }
+    }
+    
+}
